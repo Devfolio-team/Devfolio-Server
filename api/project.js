fa6dd408b8eea40463f6978ad4e6ea8fb7d7c276
HaJunRyu <hjr4557@naver.com>
2021-04-21 14:17:34 +0900

    Refactor: project라우터와 projects라우터를 나누어 주었음 modified:   api/index.js modified:   api/project.js new file:   api/projects.js

--- api/project.js
+++ api/project.js
@@ -1,27 +1,7 @@
 const express = require('express');
-const { fetchProjects, createProject, getAuthorInfo, getProjectLikeCount } = require('../dao/projectDAO');
+const { createProject } = require('../dao/projectDAO');
 
 const router = express.Router();
-
-router.get('/', async (req, res) => {
-  try {
-    const projects = await fetchProjects();
-
-    const projectsData = await Promise.all(
-      projects.map(async project => {
-        const [authorInfo] = await getAuthorInfo(project.user_user_id);
-        const [{ likeCount }] = await getProjectLikeCount(project.project_id);
-        const { nickname, profile_photo } = authorInfo;
-        return { ...project, nickname, profile_photo, likeCount };
-      })
-    );
-
-    res.status(200).json({ responseMessage: 'success', projectsData });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ responseMessage: 'failure', error });
-  }
-});
 
 router.post('/', async (req, res) => {
   const result = await createProject(req.body);
---
+++ api/projects.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { fetchProjects, getAuthorInfo, getProjectLikeCount } = require('../dao/projectDAO');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const projects = await fetchProjects();
+
+    const projectsData = await Promise.all(
+      projects.map(async project => {
+        const [authorInfo] = await getAuthorInfo(project.user_user_id);
+        const [{ likeCount }] = await getProjectLikeCount(project.project_id);
+        const { nickname, profile_photo } = authorInfo;
+        return { ...project, nickname, profile_photo, likeCount };
+      })
+    );
+
+    res.status(200).json({ responseMessage: 'success', projectsData });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ responseMessage: 'failure', error });
+  }
+});
+
+module.exports = router;
