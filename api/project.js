637aafe7f5e13afc9eba125e6f3c8a7da3b8de79
HaJunRyu <hjr4557@naver.com>
2021-04-23 14:50:07 +0900

    Feat: 특정 프로젝트 아이디로 프로젝트 조회시 좋아요 갯수를 데이터에 추가
    modified:   api/project.js

--- api/project.js
+++ api/project.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createProject, getProject, getProjectTechStacks } = require('../dao/projectDAO');
+const { createProject, getProject, getProjectTechStacks, getProjectLikeCount } = require('../dao/projectDAO');
 
 const router = express.Router();
 
@@ -10,6 +10,8 @@ router.get('/:project_id', async (req, res) => {
 
   try {
     const [projectData] = await getProject(project_id);
+    const [projectLikeCount] = await getProjectLikeCount(project_id);
+    projectData.likeCount = projectLikeCount.likeCount;
     projectPageData = { projectData };
     try {
       const projectTechStacks = await getProjectTechStacks(project_id);
