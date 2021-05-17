1e7012dac5bff8d1be34b20c41f6306b35e3e320
HaJunRyu <hjr4557@naver.com>
2021-05-18 02:16:39 +0900

    Feat: requestTechStacks테이블의 데이터를 불러올 수 있는 api추가
    Close: #28
    
    modified:   api/requestTechStacks.js
    modified:   dao/techStacksDAO.js

--- api/requestTechStacks.js
+++ api/requestTechStacks.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { requestTechStacks } = require('../dao/techStacksDAO');
+const { requestTechStacks, getRequestTechStacks } = require('../dao/techStacksDAO');
 
 const router = express.Router();
 
@@ -7,6 +7,15 @@ router.post('/', async (req, res) => {
   try {
     const { stack_name } = req.query;
     const techStacks = await requestTechStacks(stack_name);
+    res.status(200).json({ responseMessage: 'success', techStacks });
+  } catch (error) {
+    res.status(500).json({ responseMessage: 'failure' });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const techStacks = await getRequestTechStacks();
     res.status(200).json({ responseMessage: 'success', techStacks });
   } catch (error) {
     res.status(500).json({ responseMessage: 'failure' });
--- dao/techStacksDAO.js
+++ dao/techStacksDAO.js
@@ -4,3 +4,6 @@ exports.fetchTechStacks = async () => await mysqlQuery('SELECT * FROM tech_stack
 
 exports.requestTechStacks = async stackName =>
   await mysqlQuery('INSERT INTO request_tech_stacks(stack_name) value((?))', [stackName]);
+
+exports.getRequestTechStacks = async () =>
+  await mysqlQuery('SELECT * FROM request_tech_stacks ORDER BY stack_name', []);
